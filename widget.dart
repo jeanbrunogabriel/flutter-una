368e0e50226134ab7544b74452096bb108e51ab2
jeanbraga <jeanb@most.com.br>
2024-11-04 18:11:20 -0300

    First commit

---
+++ widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ),
+);
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.home),
+        title: const Text('Página Inicial'),
+      ),
+      body: Center(
+        child: Image.asset('assets/giphy.webp'), 
+      ),
+    );
+  }
+}
---
+++ widget_one.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ),
+);
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      appBar: AppBar(
+        leading: const Icon(Icons.home),
+        title: const Text('Página Inicial'),
+      ),
+      body: Center(
+        child: SizedBox(
+          width: 250,
+          height: 250,
+          child: Stack(
+            textDirection: TextDirection.ltr,
+            children: <Widget>[
+              // Background container
+              Container(
+                width: 250,
+                height: 250,
+                color: Colors.white,
+              ),
+
+              // Container with gradient and text
+              Container(
+                padding: const EdgeInsets.all(5.0),
+                alignment: Alignment.bottomCenter,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: <Color>[
+                      Colors.black.withAlpha(0),
+                      Colors.black12,
+                      Colors.black45,
+                    ],
+                  ),
+                ),
+                child: const Text(
+                  "Google Flutter",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+              ),
+
+              // GIF image
+              Image.asset(
+                'assets/giphy.webp', // Replace with the actual path to your GIF
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        tooltip: "Exemplo de botão",
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
